5aeeb96cd9ea5fa4a3573a0864aa20783db61d5a
DB_BOY <ginngi_wang@163.com>
2018-03-30 20:13:43 +0800

    参数

--- src/function/kw_args.py
+++ src/function/kw_args.py
@@ -34,4 +34,39 @@ def person(name, age, *args, city, job):
     print(name, age, args, city, job)
 
 
-person('Jack', 24, 'Beijing', 'Engineer')
+# person('Jack', 24, 'Beijing', 'Engineer')
+
+
+def person(name, age, *, city='Beijing', job):
+    print(name, age, city, job)
+
+
+person('Jack', 24, job='Engineer')
+
+
+def person(name, age, city, job):
+    # 缺少 *，city和job被视为位置参数
+    pass
+
+
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+
+f1(1, 2)
+f1(1, 2, c=3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99)
+f2(1, 2, d=99, ext=None)
+
+args = (1, 2, 3, 4)
+kw = {'d': 99, 'x': '#'}
+f1(*args, **kw)
+
+args = (1, 2, 3)
+kw = {'d': 88, 'x': '#'}
+f2(*args, **kw)

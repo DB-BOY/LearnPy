1a27043894ee82efb9a541958943b294699481dc
DB_BOY <ginngi_wang@163.com>
2018-04-12 14:46:51 +0800

    匿名函数

--- src/functional/anonymous.py
+++ src/functional/anonymous.py
@@ -3,3 +3,14 @@
 
 l = list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
 print(l)
+
+l = lambda x: x * x
+print(l)
+
+
+def build(x, y):
+    return lambda: x * x + y * y
+
+
+l = build(3, 3)
+print(l())
--- src/functional/training.py
+++ src/functional/training.py
@@ -28,3 +28,17 @@ if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
     print('测试通过!')
 else:
     print('测试失败!')
+
+
+##改造匿名函数
+# def is_odd(n):
+#     return n % 2 == 1
+#
+def is_odd(n):
+    l = lambda: (n % 2 == 1)
+    return l()
+
+
+L = list(filter(is_odd, range(1, 20)))
+
+print(L)

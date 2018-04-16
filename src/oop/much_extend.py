995a3eed080a7b70e8ce85b52921885ecb35189a
DB_BOY <ginngi_wang@163.com>
2018-04-16 18:01:10 +0800

    多继承

---
+++ src/oop/much_extend.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# 多继承
+class Animal(object):
+    pass
+
+
+# 大类:
+class Mammal(Animal):
+    pass
+
+
+class Bird(Animal):
+    pass
+
+
+# 各种动物:
+# class Dog(Mammal):
+#     pass
+#
+#
+# class Bat(Mammal):
+#     pass
+#
+#
+# class Parrot(Bird):
+#     pass
+#
+#
+# class Ostrich(Bird):
+#     pass
+
+
+class Runnable(object):
+    def run(self):
+        print('Running...')
+
+
+class Flyable(object):
+    def fly(self):
+        print('Flying...')
+
+
+class Dog(Mammal, Runnable):
+    pass
+
+
+class Bat(Mammal, Flyable):
+    pass
+
+
+d = Dog()
+d.run()
+
+# MixIn
